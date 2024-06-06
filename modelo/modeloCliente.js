ec21a95c9aa96109a90273c249539751672a422f
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2024-06-06 19:24:24 -0300

    Add files via upload

---
+++ modelo/modeloCliente.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+const conexao = require('../conexao/conexao');
+const Endereco = require('../endereco/modeloEndereco')
+
+const Usuario = conexao.define('Usuarios', {
+    codUsuario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+    },
+    CPF:{
+        type: Sequelize.STRING(11),
+        allowNull: false,
+        unique: true
+    },
+    CNPJ:{
+        type: Sequelize.STRING(14),
+        allowNull: false
+    },
+    dataNascimento:{
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    codEndereco: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: Endereco,
+            key: 'codEndereco',
+        },
+        onDelete: 'CASCADE'   
+    }
+}, {
+    timestamps: false
+});
+
+Usuario.sync({ force: false });
+
+module.exports = Usuario;
